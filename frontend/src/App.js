82efb5f84a8be3a8bbffd357bbd2a75d74962d26
Fahad Shaikh <fahadshaikh9776@gmail.com>
2020-11-20 15:42:48 +0530

    Added Components & Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Routes from './Routes/Routes';
 
 function App() {
     return (
         <div className='App'>
             <h1>Company-Employee-Records</h1>
+            <Routes />
         </div>
     );
 }
---
+++ frontend/src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../Components/Login';
+import Register from '../Components/Register';
+import Dashboard from '../Components/Dashboard';
+import Details from '../Components/Details';
+
+export const Routes = () => {
+    return (
+        <>
+            <Switch>
+                <Route path='/' exact render={() => <Login />} />
+                <Route path='/SignUp' render={() => <Register />} />
+                <Route path='/Dashboard' render={() => <Dashboard />} />
+                <Route path='/Details' render={() => <Details />} />
+                {/* <Route
+            path="/CheckoutPage"
+            render={(props) => <CheckoutPage {...props} />}
+          /> */}
+            </Switch>
+        </>
+    );
+};
